aa3af232a92a6b3456f348691a81d907317e27bf
Anandh15 <subhaanandh.s@gmail.com>
2025-09-04 21:24:38 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author Vikram Pal
+ * Main application class for Spring Boot application.
+ * This class bootstraps the application.
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
